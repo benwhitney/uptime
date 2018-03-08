fa233b1dd45deade61f62f251babfb5720213467
benwhitney78 <benwhitney78@hotmail.com>
2018-03-08 00:25:01 -0500

    Reverted changes

--- uptime-app/src/app/uptime-status/uptime-status.component.ts
+++ uptime-app/src/app/uptime-status/uptime-status.component.ts
@@ -1,5 +1,6 @@
 declare var require: any;
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { UptimeService } from '../uptime-service/uptime.service';
 import { UptimeStatus } from '../class/outage.class';
 
 @Component({
@@ -9,12 +10,29 @@ import { UptimeStatus } from '../class/outage.class';
 })
 export class UptimeStatusComponent {
 	
-	@Input() public status: UptimeStatus;
+	protected status: UptimeStatus;
 	protected minutes: number;
+	protected moment: any; 
 	protected fromNow: string;
 	protected changeDate: Date;
 
-	constructor() {		
-	
+	constructor(protected service: UptimeService) {		
+		this.getData();
+		this.moment = require('moment');
+	}
+
+	protected getData() {
+		this.service.getStatus().subscribe((data) => {
+			this.updateStatus(data as UptimeStatus);
+		});
+	}
+
+	protected updateStatus(status: UptimeStatus) {
+		this.status = status;
+		if (status.online) {
+			this.fromNow = this.moment(status.outageEnd).fromNow();
+		} else {
+			this.fromNow = this.moment(status.outageStart).fromNow();			
+		}
 	}
 }
