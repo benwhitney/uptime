0d0663aa5c4333acdf7b523c2fa2f162f20bc92d
Ben Whitney <benwhitney78@hotmail.com>
2018-03-14 21:30:22 -0400

    Added uptime counter

--- uptime-app/src/app/uptime-status/uptime-status.component.ts
+++ uptime-app/src/app/uptime-status/uptime-status.component.ts
@@ -2,6 +2,7 @@ declare var require: any;
 import { Component } from '@angular/core';
 import { UptimeService } from '../uptime-service/uptime.service';
 import { UptimeStatus } from '../class/outage.class';
+import { uptime } from 'os';
 
 @Component({
     selector: 'uptime-status',
@@ -15,12 +16,41 @@ export class UptimeStatusComponent {
 	protected moment: any; 
 	protected fromNow: string;
 	protected changeDate: Date;
+	protected uptime: string;
 
 	constructor(protected service: UptimeService) {		
 		this.getData();
 		this.moment = require('moment');
 		setInterval(() => { this.getData() }, 60000);
+		setInterval(() => { this.tick()}, 1000);
 	}
+
+	protected tick() {
+		let target;
+		if (this.status.online) {
+			target = new Date(this.status.outageEnd);
+		} else {
+			target = new Date(this.status.outageStart);
+		}
+		let now = new Date();
+		let secondsDiff = Math.floor((now.getTime() - target.getTime()) / 1000);
+
+		let minutesDiff = Math.floor(secondsDiff / 60);
+		secondsDiff -= minutesDiff * 60;
+
+		let hoursDiff = Math.floor(minutesDiff / 60);
+		minutesDiff -= hoursDiff * 60;
+
+		this.uptime = this.pad(hoursDiff) + ':' + this.pad(minutesDiff) + ':' + this.pad(secondsDiff);
+		this.updateStatus(this.status);
+	}
+
+	private pad (val: number, size? : number) {
+		var s = String(val);
+		while (s.length < (size || 2)) {s = "0" + s;}
+		return s;
+	}
+	
 
 	protected getData() {
 		this.service.getStatus().subscribe((data) => {
